d356c922bf87bdb93f220c6d1db4e48ae201adde
galoberlyn <garlejogaloberlyn@gmail.com>
2019-11-14 20:41:37 +0800

    [ADD] About section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,4 +10,29 @@ $(document).ready(function () {
 
     var typed = new Typed(".typewriter", options);
 
+    // scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
+
+});
+
+
+window.onscroll = function() { scroller() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function scroller() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
